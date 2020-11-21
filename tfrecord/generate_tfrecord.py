ae2529e2f8039e50200df652619f10b97fb18895
SerQuicky <Michael.frank96@web.de>
2020-11-21 16:32:30 +0100

    Update label resolver

--- tfrecord/generate_tfrecord.py
+++ tfrecord/generate_tfrecord.py
@@ -28,7 +28,7 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'totoro':
+    if row_label == 'shoe':
     # if row_label == 'tommad':
         return 1
     elif row_label == 'nekobus':
